fd3523b2e3de3aa4caf8fe047eb8730284775fca
Valerio Viperino <10340139+vvzen@users.noreply.github.com>
2021-08-18 09:01:52 +0100

    First commit of the arduino code

---
+++ arduino_serial_receiver/arduino_serial_receiver.ino
@@ -0,0 +1,39 @@
+#define LED_PIN 13
+
+int incomingByte = 0;
+const byte MESSAGE_MAX_LENGTH = 32;
+char currentMessage[MESSAGE_MAX_LENGTH];
+
+const int END_MESSAGE_DELIMITER = 0x3F; // '>' char delimiter
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+
+  digitalWrite(LED_PIN, LOW);
+
+  if (Serial.available() > 0) {
+
+    // Refill the buffer with nulls
+    for (int i = 0; i < MESSAGE_MAX_LENGTH; i++){
+      currentMessage[i] = '\0';
+    }
+
+    // Read into buffer
+    Serial.readBytesUntil(END_MESSAGE_DELIMITER, currentMessage, MESSAGE_MAX_LENGTH);
+
+    // Write back
+    digitalWrite(LED_PIN, HIGH);
+    for (int i = 0; i < MESSAGE_MAX_LENGTH; i++){
+      if (currentMessage[i] != '\0') {
+        Serial.write(currentMessage[i]); 
+      }
+    }
+    digitalWrite(LED_PIN, LOW);
+
+  }
+}
